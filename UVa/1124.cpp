9d88f81f7b5e310638a62e15cc7a65ec8ed4ff44
Benjamín Mellado <38161980+bmellado@users.noreply.github.com>
2018-07-20 18:18:39 -0400

    1124 accepted

---
+++ UVa/1124.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char c[1000];
+    while(gets(c)){
+        int len = strlen(c);
+        for(int i = 0; i <len; i++){
+            printf("%c",c[i]);
+        }
+        printf("\n");
+    }
+return 0;}
