29291031254578d429e3f8761e07ba4ed54a5275
Benjamín Mellado <38161980+bmellado@users.noreply.github.com>
2018-07-25 18:39:50 -0400

    Accepted

--- UVa/11727.cpp
+++ UVa/11727.cpp
@@ -1,1 +1,16 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int TC,nCase=1;
+    scanf("%d", &TC);
+    while(TC--){
+        vector<int> n;
+        int x,i=3;
+        while(i--){
+            scanf("%d", &x);
+            n.push_back(x);
+        }
+        sort(n.begin(),n.end());
+        printf("Case %d: %d\n",nCase++,n[1]);
+    }
+return 0;}
